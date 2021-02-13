3f1036f79be641d80ce226787c3ae06bd84b23ae
timberwang0810 <zilinwan@andrew.cmu.edu>
2021-02-12 21:14:46 -0500

    added drone controller

---
+++ Fragile Drone/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool facingLeft;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        facingLeft = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        if (horizontalMove < 0) facingLeft = true;
+        else if (horizontalMove > 0) facingLeft = false;
+    }
+
+}
